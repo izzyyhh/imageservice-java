b846c40bd5b9124a2f4f13f5206743b82c1ed7c6
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-05-31 01:38:16 +0200

    init spring boot

---
+++ src/test/java/com/izzy/imageservice/ImageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzy.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
